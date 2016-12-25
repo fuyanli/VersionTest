76875ee8e995aca9745c548903b8df17263ff234
fyl <1289525342@qq.com>
2016-12-25 16:52:45 +0800

    second

--- app/src/main/java/com/example/fyl/androidtest/MainActivity.java
+++ app/src/main/java/com/example/fyl/androidtest/MainActivity.java
@@ -2,9 +2,11 @@ package com.example.fyl.androidtest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
